fc24f136a3d0c86ad3688885c96c2fc7520c3645
Thornthep Chomchuen <thornthep.c@ku.th>
2021-11-28 22:16:30 +0700

    add csv

--- fetchcsv.py
+++ fetchcsv.py
@@ -1,9 +1,34 @@
 import csv
-import urllib
+import mysql.connector
 
-url = 'https://docs.google.com/spreadsheets/d/1Oqb9g4mY9L6N6UWeU4gNB-MrLmOzqaht5S3enYOswj4/edit?usp=sharing'
-response = urllib.urlopen(url)
-with open(response, newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+connection = mysql.connector.connect(host='iot.cpe.ku.ac.th',
+                                        database='b6210545963',
+                                        user='b6210545963',
+                                        password='thornthep.c@ku.th')
+
+with open('Covid-191.csv', encoding='utf-8') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count > 0:
+            province = row[1]
+            vaccine = row[3]
+            try:   
+                cursor = connection.cursor()
+                mySql_insert_query = """INSERT INTO covid_vaccinated_province (province, vaccine)
+                                        VALUES (%s, %s) """
+
+                record = (province, vaccine)
+                cursor.execute(mySql_insert_query, record)
+                connection.commit()
+                print('Success')
+
+            except mysql.connector.Error as error:
+                print("Failed to insert into MySQL table {}".format(error))
+
+        line_count +=1
+
+if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
